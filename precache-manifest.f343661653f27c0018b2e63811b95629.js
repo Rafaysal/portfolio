21558084675176993f022738a3f60f6fa4384e09
Rafaysal <rafaysal@hotmail.com>
2019-08-08 20:07:26 -0700

    Updates

---
+++ precache-manifest.f343661653f27c0018b2e63811b95629.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600a8ee8996513bf9e1bf441c7dfd504",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3aad3fb19eeeb88d95c5",
+    "url": "/portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "70b76a4921335624c3e4",
+    "url": "/portfolio/static/css/main.132debad.chunk.css"
+  },
+  {
+    "revision": "3aad3fb19eeeb88d95c5",
+    "url": "/portfolio/static/js/2.9e66d176.chunk.js"
+  },
+  {
+    "revision": "70b76a4921335624c3e4",
+    "url": "/portfolio/static/js/main.78072450.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "f251f4b14dd7a08869e4faaca06ec241",
+    "url": "/portfolio/static/media/bg.f251f4b1.jpg"
+  },
+  {
+    "revision": "54c78590b3237e0bd4925e4daa209c3e",
+    "url": "/portfolio/static/media/call.54c78590.jpeg"
+  },
+  {
+    "revision": "89a2fb8814e213cbe98691d4656ddbb0",
+    "url": "/portfolio/static/media/codecademy.89a2fb88.png"
+  },
+  {
+    "revision": "e831279730182dc0f53fae53b3ae8532",
+    "url": "/portfolio/static/media/cv.e8312797.jpg"
+  },
+  {
+    "revision": "1876d8f89b4f1a7b5680c27b2cedc2ce",
+    "url": "/portfolio/static/media/gmail.1876d8f8.png"
+  },
+  {
+    "revision": "a330e248626552a23af35e5c46526234",
+    "url": "/portfolio/static/media/linkedin.a330e248.png"
+  },
+  {
+    "revision": "7bc78205912c5ba9e3e6cfaa7e1c7803",
+    "url": "/portfolio/static/media/logo.7bc78205.jpg"
+  },
+  {
+    "revision": "08b891b74af5a774a7e2bd0261e9eeeb",
+    "url": "/portfolio/static/media/mcgill.08b891b7.jpeg"
+  },
+  {
+    "revision": "e14d9690dfc095098102b5d0c27a5fa6",
+    "url": "/portfolio/static/media/rafayFinal.e14d9690.png"
+  }
+]);
